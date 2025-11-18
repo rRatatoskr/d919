014d483fbc1d78940791257b48c8117307780ad4
rRatatoskr <iznt49@icloud.com>
2025-11-19 04:10:08 +0900

    GithuPagesが反映されない

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-//  basePath: process.env.NODE_ENV === 'production' ? '/d919' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/d919' : '',
 };
 
 export default nextConfig;
